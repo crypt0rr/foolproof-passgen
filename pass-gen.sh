ecd356d1cd5cbcf1732949a4aa1448240fa08c52
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-01-05 15:50:53 +0100

    Small optimisation

--- pass-gen.sh
+++ pass-gen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CHARS="abcdefghjkmnpqrstuvwxyz123456789ABCDEFGHJKLMNPQRSTUVWXYZ!?"
-END=$1
-for i in $(seq 1 $END); do
+for i in $(seq 1 $1); do
     echo -n "${CHARS:RANDOM%${#CHARS}:1}"
 done
 echo
