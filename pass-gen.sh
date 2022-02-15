d83bcb83b319e6761edfa4cddb192d7e847b1271
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-02-15 20:16:19 +0100

    Removed '1' - more better

--- pass-gen.sh
+++ pass-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CHARS="abcdefghjkmnpqrstuvwxyz123456789ABCDEFGHJKLMNPQRSTUVWXYZ!?"
+CHARS="abcdefghjkmnpqrstuvwxyz23456789ABCDEFGHJKLMNPQRSTUVWXYZ!?"
 for i in $(seq 1 $1); do
     echo -n "${CHARS:RANDOM%${#CHARS}:1}"
 done
